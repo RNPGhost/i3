12569abf097ca69b43cda29d11a9960e097da89d
Sam Hutton <sfh@corefiling.com>
2017-07-26 17:50:44 +0100

    add workspace sorting

--- i3_workspace_controller.py
+++ i3_workspace_controller.py
@@ -7,12 +7,17 @@ NAME_SEPERATOR = '-'
 
 def create_default_workspaces():
   for i in range(get_workspaces().__len__(), 0, -1):
-    subprocess.Popen(["i3-msg", "workspace " + str(i)])
-    newWorkspaceName = get_default_workspace_name(i)
-    subprocess.Popen(["i3-msg", "workspace " + newWorkspaceName])
+    subprocess.Popen(["i3-msg", "rename workspace " + str(i) + " to " + get_default_workspace_name(i)])
 
-def move_to_workspace(workspaceNumber, moveContainer=False, focus=False):
-  targetWorkspaceName = get_target_workspace_name(workspaceNumber)
+def move_to_workspace(targetWorkspace, moveContainer=False, focus=False):
+  targetWorkspaceNumber = targetWorkspace
+  if targetWorkspace == 'next':
+    targetWorkspaceNumber = get_current_workspace_number() + 1
+  elif targetWorkspace == 'prev':
+    targetWorkspaceNumber = get_current_workspace_number() - 1
+    if targetWorkspaceNumber == 0:
+      return
+  targetWorkspaceName = get_target_workspace_name(targetWorkspaceNumber)
   if moveContainer:
     subprocess.Popen(["i3-msg", "move to workspace " + targetWorkspaceName])
   if focus:
@@ -29,10 +34,11 @@ def get_workspaces():
   return arr
 
 def get_default_workspace_name(monitorNumber):
-  return (chr(64 + monitorNumber) + NAME_SEPERATOR + "1")
+  return ("1:" + chr(64 + monitorNumber) + NAME_SEPERATOR + "1")
 
 def get_target_workspace_name(workspaceNumber):
-  return get_focused_workspace().split(NAME_SEPERATOR)[0] + NAME_SEPERATOR + str(workspaceNumber)
+  print(workspaceNumber)
+  return str(workspaceNumber) + ":" + get_focused_workspace().split(':')[1].split(NAME_SEPERATOR)[0] + NAME_SEPERATOR + str(workspaceNumber)
 
 def get_focused_workspace():
   handle = subprocess.Popen(["i3-msg","-t","get_workspaces"], stdout=subprocess.PIPE)
@@ -42,6 +48,13 @@ def get_focused_workspace():
   for i in data:
     if(i['focused']):
       return i['name']
+
+def get_current_workspace_number():
+  return get_workspace_number(get_focused_workspace())
+
+def get_workspace_number(workspaceName):
+  print("current workspace number is " + workspaceName.split(NAME_SEPERATOR)[1])
+  return int(workspaceName.split(NAME_SEPERATOR)[1])
 
 def enough_arguments(requiredNumberOfArguments):
   enoughArguments = (len(sys.argv) >= requiredNumberOfArguments)
@@ -56,15 +69,15 @@ if enough_arguments(2):
       create_default_workspaces()
   elif command == 'focus':
     if enough_arguments(3):
-      workspaceNumber = sys.argv[2]
-      move_to_workspace(workspaceNumber, focus=True)
+      workspace = sys.argv[2]
+      move_to_workspace(workspace, focus=True)
   elif command == 'move':
     if enough_arguments(3):
-      workspaceNumber = sys.argv[2]
-      move_to_workspace(workspaceNumber, moveContainer=True)
+      workspace = sys.argv[2]
+      move_to_workspace(workspace, moveContainer=True)
   elif command == 'move_and_focus':
     if enough_arguments(3):
-      workspaceNumber = sys.argv[2]
-      move_to_workspace(workspaceNumber, moveContainer=True, focus=True)
+      workspace = sys.argv[2]
+      move_to_workspace(workspace, moveContainer=True, focus=True)
   else:
     print("Error: Command '" + command + "' not recognised")
