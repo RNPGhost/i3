cd5b2bfe5f6eb6902c03924474e27af4fe3612e2
Sam Hutton <sfh@corefiling.com>
2017-08-10 17:15:54 +0100

    add color to muted volumes

--- home_volume.sh
+++ home_volume.sh
@@ -2,4 +2,9 @@
 
 SOUND_DEVICE_NAME="$(~/.config/i3/output_sink_controller.py get)"
 VOLUME="$(/usr/local/libexec/i3blocks/volume)"
-echo -n "$SOUND_DEVICE_NAME: $VOLUME"
+if [ $VOLUME = "MUTE" ]; then
+    VOLUME="<span color='red'>Muted</span>"
+elif [ $VOLUME = "0%" ]; then
+    VOLUME="<span color='red'>$VOLUME</span>"
+fi
+echo "$SOUND_DEVICE_NAME: $VOLUME"
